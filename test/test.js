02494949c2899331d6933edece6d0d5551cf668e
Dan Wager <danielwager@gmail.com>
2016-09-14 17:06:41 -0700

    Rename test function

--- test/test.js
+++ test/test.js
@@ -3,34 +3,34 @@ var expect = require('chai').expect;
 var buildQuery = require('../lib/queryBuilder');
 var data = require('./test.data');
 
-function expectBuilder (data) {
+function expectBuilderEql (data) {
     expect(buildQuery(data.query)).to.eql(data.result);
 }
 
 describe('with no arguments', () => {
     it('should return non-scoped query when passed undefined', () => {
-        expectBuilder(data.undef);
+        expectBuilderEql(data.undef);
     });
 
     it('should return non-scoped query when passed {}', () => {
-        expectBuilder(data.empty);
+        expectBuilderEql(data.empty);
     });
 });
 
 describe('with single-level queryFilter', () => {
     it('should handle a simple KV pair', () => {
-        expectBuilder(data.single.simple);
+        expectBuilderEql(data.single.simple);
     });
 
     it('should handle a spaces in the key', () => {
-        expectBuilder(data.single.space);
+        expectBuilderEql(data.single.space);
     });
 
     it('should handle implicit $and', () => {
-        expectBuilder(data.single.implicitAnd);
+        expectBuilderEql(data.single.implicitAnd);
     });
 
     it('should handle explicit $and', () => {
-        expectBuilder(data.single.explicitAnd);
+        expectBuilderEql(data.single.explicitAnd);
     });
 });
