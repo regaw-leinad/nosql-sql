b6451724f70634b92e0a18230a56b133713a5937
Dan Wager <danielwager@gmail.com>
2016-09-14 18:27:36 -0700

    Move nested index to main index

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./lib');
+var QueryBuilder = require('./lib/queryBuilder');
+
+/**
+ * Builds a DocumentDB query object from MongoDB-style syntax
+ *
+ * @param queryFilter {Object} The MongoDB-style object to build query from
+ * @return {DocDBQuery} The DocumentDB query object
+ */
+module.exports = function buildQuery (queryFilter) {
+    return new QueryBuilder(queryFilter).build();
+};
