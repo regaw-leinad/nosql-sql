a1c9d9195f9aa2cef9c49d74921b32d2df47127d
Dan Wager <danielwager@gmail.com>
2016-09-14 18:31:27 -0700

    Why not just use ES5 for ALLLLL??!?

--- test/test.js
+++ test/test.js
@@ -11,48 +11,48 @@ function expectBuilderThrow (data, errorMsg) {
     expect(buildQuery.bind(this, data.query)).to.throw(errorMsg);
 }
 
-describe('with no arguments', () => {
-    it('should return non-scoped query when passed undefined', () => {
+describe('with no arguments', function () {
+    it('should return non-scoped query when passed undefined', function () {
         expectBuilderEql(data.undef);
     });
 
-    it('should return non-scoped query when passed {}', () => {
+    it('should return non-scoped query when passed {}', function () {
         expectBuilderEql(data.empty);
     });
 });
 
-describe('with single-level queryFilter', () => {
-    it('should handle a simple KV pair', () => {
+describe('with single-level queryFilter', function () {
+    it('should handle a simple KV pair', function () {
         expectBuilderEql(data.single.simple);
     });
 
-    it('should handle a spaces in the key', () => {
+    it('should handle a spaces in the key', function () {
         expectBuilderEql(data.single.space);
     });
 
-    it('should handle implicit $and', () => {
+    it('should handle implicit $and', function () {
         expectBuilderEql(data.single.implicitAnd);
     });
 
-    it('should handle explicit $and', () => {
+    it('should handle explicit $and', function () {
         expectBuilderEql(data.single.explicitAnd);
     });
 
-    it('should throw immediately-nested $and', () => {
+    it('should throw immediately-nested $and', function () {
         expectBuilderThrow(data.single.explicitAndThrow, 'Incorrect syntax using nested $and')
     });
 
-    it('should handle $in', () => {
+    it('should handle $in', function () {
         expectBuilderEql(data.single.inClause);
     });
 
-    it('should handle $between', () => {
+    it('should handle $between', function () {
         expectBuilderEql(data.single.between);
     });
 });
 
-describe('with a nested queryFilter', () => {
-    it('should throw on an invalid nested clause', () => {
+describe('with a nested queryFilter', function () {
+    it('should throw on an invalid nested clause', function () {
         expectBuilderThrow(data.nested.invalid, 'Nested keys must be clause: $notClause');
     });
 });
